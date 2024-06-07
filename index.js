b2723397f0cf439cd5e27e8bac495fdbae036382
IBARAM1917 <abiraminagaraj22@gmail.com>
2024-06-07 13:25:41 +0530

    updated

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
+//import section
 import express from 'express';
-import fs from fs;
-import { format } from 'date-fns';
+import fs from 'fs';
+import { format } from "date-fns";
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+//decleration/initialization
+const app = express();
+const PORT = 4000;
 
+// Utility to get dirname 
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
-const app =express();
-const PORT =4000;
-
-const filename=fileURLToPath(import.meta,url);
-const dirname=path.dirname(filename);
-
-
+//middleware
 app.use(express.json());
 
+
+//default routes
 app.get('/',(req,res)=>{
-    res.status(200).send('Welcome to Nodejs File System,If you want to create Timestamp give/create or If you want to read the created Timestamp give /read along with the Render url')
-    
+  res.status(200).send('Welcome to Nodejs File System, If you want to create Timestamp give /create or If you want to read the created Timestamp give /read along with the Render url')
 })
+//create route
 app.get('/create',(req,res)=>{
-    let toDay =format(new Date(),'dd-MM-yyyy-HH-mm-ss')
-
-    const filePath =`Timestamp/${toDay}.txt`
-    fs.writeFilesync(filePath,`${toDay}`,'utf8')
-    res.status(200).send(`Tmestamp are created successfull ${toDay}`)
+    let toDay = format(new Date(),'dd-MM-yyyy-HH-mm-ss')
+    // console.log(toDay);
+    const filePath = `Timestamp/${toDay}.txt`
+    fs.writeFileSync(filePath,`${toDay}`,'utf8')
+    res.status(200).send(`Timestamp are created successfully ${toDay}`)
 })
-app.get('/read',(req,res)=>{
-    const directoryPath=path.join(dirname,'./Timestamp');
-    let fileData=[];
-    fs.readdirsync(directoryPath).forEach(file=>{
-        const filePath=path.join(directoryPath,file);
-        const data=fs.readdirsync(filePath,'utf8');
-        fileData.push({file,data});
+//read route
+app.get('/read', (req, res) => {
+    const directoryPath = path.join(dirname, './Timestamp');
+    let fileData = [];
+  
+     fs.readdirSync(directoryPath).forEach(file => {
+      const filePath = path.join(directoryPath, file);
+      const data = fs.readFileSync(filePath, 'utf8');
+      fileData.push( {file, data} );
     });
-    res.status(200).send(fileData);
-});
+  res.status(200).send(fileData);
+  });
 
+//running port
 app.listen(PORT,()=>{
     console.log("Server is running");
 })
